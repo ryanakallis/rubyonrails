a481a6f439f602cd1ca1794bc203e8ff1158c49b
Ryan Kallis <ryan.kallis@vulcanlabs.com>
2021-06-22 15:33:01 +0200

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "invoices#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/invoices", to: "invoices#index"
 end
